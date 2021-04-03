9ac6a956250ca670145a14426ee7313341f0831a
Jean28518 <jf_v@gmx.de>
2021-04-03 19:40:33 +0200

    Testing

---
+++ OtherTileSets/Atlas/terrain_atlas.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="terrain_atlas" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="terrain_atlas.png" width="1024" height="1024"/>
+ <tile id="169">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.0547" y="0.412797" width="25.7585" height="31.5377"/>
+   <object id="2" x="7.5129" y="-11.4757"/>
+  </objectgroup>
+ </tile>
+ <tile id="366">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.5676" y="10.485" width="20.8875" height="20.9701"/>
+   <object id="2" x="0.247678" y="0.412797" width="31.1249" height="30.9598"/>
+  </objectgroup>
+ </tile>
+</tileset>
