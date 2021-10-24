1ec7574b9c2645dec5eb059a0bed946274f752ce
Jean28518 <39700889+Jean28518@users.noreply.github.com>
2021-10-24 09:25:00 +0000

    Deploying to gh-pages from master @ 7b789e36e314094b8075b744020f602551a23275 🚀

---
+++ tilesets/restaurant.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="restaurant" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="../tilesets/restaurant.png" width="134" height="72"/>
+</tileset>
---
+++ tilesets/ukulele.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="ukulele" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../tilesets/ukulele.png" width="128" height="128"/>
+</tileset>
