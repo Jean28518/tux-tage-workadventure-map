7b789e36e314094b8075b744020f602551a23275
Jean28518 <jf_v@gmx.de>
2021-10-24 11:24:28 +0200

    WIP

---
+++ tilesets/rc3-minideko.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="rc3-minideko" tilewidth="32" tileheight="32" tilecount="133" columns="7">
+ <image source="rc3-minideko.png" width="224" height="608"/>
+</tileset>
---
+++ tilesets/rc3-mullana-objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="rc3-mullana-objects" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="../tilesets/rc3-mullana-objects.png" width="512" height="256"/>
+</tileset>
